941c8456b68365e0ae67546d365b5a1e41c04fc9
maheshtheril <maheshtheril@users.noreply.github.com>
2026-01-06 12:56:45 +0530

    fix(build): remove duplicate Loader2 definition

--- src/components/prescriptions/prescription-editor.tsx
+++ src/components/prescriptions/prescription-editor.tsx
@@ -1064,24 +1064,7 @@ function Stethoscope(props: any) {
     )
 }
 
-function Loader2(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-        </svg>
-    )
-}
+
 
 function Receipt(props: any) {
     return (
