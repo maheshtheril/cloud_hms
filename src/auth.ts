4e023301034efd7500334894e54525254eca4a40
maheshtheril <maheshtheril@users.noreply.github.com>
2026-01-19 05:08:05 +0530

    Enable trustHost for NextAuth

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     ...authConfig,
+    trustHost: true,
     providers: [
         Credentials({
             credentials: {
